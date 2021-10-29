7e11d8f8c051e278b0d42cb94534443e5884bfed
BuildTools <elianpaludi@gmail.com>
2021-10-29 12:58:24 -0300

    animaciones

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,14 +1,6 @@
 let cursor = document.getElementById('cursor'); //Cursor
 
 
-document.addEventListener('mousemove', function(e) {
-    cursor.style.top = e.pageY + 'px';
-    cursor.style.left = e.pageX + 'px';
-
-    console.log(e.pageX, e.pageY);
-});
-
-
 var typed = new Typed('#typed',{
     strings:["proyecto","sueño","idea", "pagina web"],
     backSpeed: 70,
