fdebeb73e2ca0b6a578093ddd37bf5853eda215b
BuildTools <elianpaludi@gmail.com>
2021-10-29 12:12:37 -0300

    update

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,4 +4,13 @@ let cursor = document.getElementById('cursor'); //Cursor
 document.addEventListener('mousemove', function(e) {
     cursor.style.top = e.pageY + 'px';
     cursor.style.left = e.pageX + 'px';
+
+    console.log(e.pageX, e.pageY);
 });
+
+
+var typed = new Typed('#typed',{
+    strings:["proyecto","sueño","idea", "pagina web"],
+    backSpeed: 70,
+    typeSpeed: 40
+    });
